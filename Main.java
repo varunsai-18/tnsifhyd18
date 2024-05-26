ed2520ad18b4f9a91c7452f39d45233095b3559b
varunsai-18 <168673261+varunsai-18@users.noreply.github.com>
2024-05-26 14:24:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package approach2;
+
+public class Main {
+	public static void main(String[] args) {
+		int y=10;
+		appr2 a=new appr2();
+		System.out.println(a.x);
+		System.out.println(appr2.name);
+		System.out.println(y);
+	}	
+}
---
+++ appr2.java
@@ -0,0 +1,8 @@
+package approach2;
+
+public class appr2 {
+	int x=2;
+    static String name="hello";
+
+	}
+
