d83c39731175b0632cc852fdfd7d53f31fa879d2
varunsai-18 <168673261+varunsai-18@users.noreply.github.com>
2024-05-26 14:22:45 +0530

    Add files via upload

---
+++ appr1.java
@@ -0,0 +1,15 @@
+package approach1;
+
+public class appr1 {
+		public int x=10;
+		static String name="Varun";
+		public static void main(String[] args) {
+	     double y= 18.7;
+	     System.out.println(y);
+	     appr1 a1=new appr1();
+	     System.out.println(a1.x);
+	     System.out.println(appr1.name);
+		}
+
+	}
+
